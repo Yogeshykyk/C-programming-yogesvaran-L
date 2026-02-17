2c70c3620772e45a45cfad841ca981081329edbf
Yogeshykyk <yogeshkaviyk@gmail.com>
2026-02-17 10:34:42 +0530

    Create function.c

---
+++ function.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int findMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
+    return max;
+}
+
+int findMin(int arr[], int n) {
+    int min = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] < min)
+            min = arr[i];
+    return min;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Max: %d\n", findMax(arr, n));
+    printf("Min: %d", findMin(arr, n));
+
+    return 0;
+}
